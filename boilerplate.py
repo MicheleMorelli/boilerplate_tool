4a7290b9bf01101f71d02ccc51a2821fccdad27d
MicheleMorelli <michele.morelli@london.ac.uk>
2018-02-15 17:12:21 +0000

    first commit

---
+++ boilerplate.py
@@ -0,0 +1,18 @@
+'''
+Quick utility to add a boilerplate to an existing file (from another template file).
+
+$Python3 boilerplate.py [file name]
+
+'''
+import sys
+
+files = ['boiler.txt', sys.argv[1],'end_boiler.txt']
+text = []
+
+for i in files:
+        with open(i, 'r') as fh:
+                text.extend(fh.readlines())
+with open(sys.argv[1], 'w') as fh:
+        for line in text:
+                fh.write(line)
+print ("done!")
